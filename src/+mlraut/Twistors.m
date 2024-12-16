75c012fcf4de5cfe01e3ef45ffb3424a82c73521
jjleewustledu <16329959+jjleewustledu@users.noreply.github.com>
2024-12-15 23:22:14 -0600

    added Twistors class for complex time-series structures

---
+++ src/+mlraut/Twistors.m
@@ -0,0 +1,204 @@
+classdef Twistors < handle & mlsystem.IHandle
+    %% line1
+    %  line2
+    %  
+    %  Created 11-Dec-2024 22:23:24 by jjlee in repository /Users/jjlee/MATLAB-Drive/mlraut/src/+mlraut.
+    %  Developed on Matlab 24.2.0.2773142 (R2024b) Update 2 for MACA64.  Copyright 2024 John J. Lee.
+    
+    properties (Dependent)
+        bold_signal  % hilbert(bold) ~ Nt x Nx
+        physio_signal  % hilbert(physio) ~ Nt x 1
+    end
+
+    methods  %% GET
+        function g = get.bold_signal(this)
+            g = this.ihcp_.bold_signal;
+        end
+        function g = get.physio_signal(this)
+            g = this.ihcp_.physio_signal;
+        end
+    end
+
+    methods
+
+        function [pos,v] = center_of_mass_position(this, roi)
+            %% pos ~ 3 x 1, in mm
+            %  v ~ 3 x 1, indices of imaging img
+
+            arguments
+                this mlraut.Twistors
+                roi mlfourd.ImagingContext2 = this.ihcp_.roi
+            end
+
+            if strcmp(roi.stateTypeclass, "mlfourd.TrivialTool")
+                pos = [0; 0; 0];
+                v = [1; 1; 1];
+                return
+            end
+
+            % Gemini Advanced 1.5 Pro, 20241211 230000
+            % I have a 3D array in matlab representing a grid of positions in space. The array contains numbers
+            % describing a cluster of massive objects. How can I quickly calculate position of the center of mass of the
+            % cluster?
+
+            mass_array = double(roi);
+
+            % Define grid dimensions
+            x_dim = size(mass_array, 1);
+            y_dim = size(mass_array, 2);
+            z_dim = size(mass_array, 3);
+
+            % Create coordinate grids
+            [y_coords, x_coords, z_coords] = meshgrid(1:y_dim, 1:x_dim, 1:z_dim);
+
+            % Total mass of the cluster
+            total_mass = sum(mass_array(:)); 
+
+            % Calculate the weighted coordinates, center of mass
+            x_cm = sum(x_coords(:) .* mass_array(:)) / total_mass;
+            y_cm = sum(y_coords(:) .* mass_array(:)) / total_mass;
+            z_cm = sum(z_coords(:) .* mass_array(:)) / total_mass;
+
+            v = [x_cm; y_cm; z_cm];
+            pos = this.voxel_indices_to_position(v, roi);
+        end
+
+        function pos = grayordinate_positions(this)
+            %% pos ~ 3 x N_grayords, in mm
+
+            % cifti has models
+            task = cifti_read(convertStringsToChars(this.ihcp_.task_dtseries_fqfn));
+            models = task.diminfo{1}.models;
+
+            % left cortex
+            gl = gifti(convertStringsToChars(this.ihcp_.cohort_data.surf_gii_fqfn("L")));
+            gl_pos = gl.vertices';
+            gl_pos = gl_pos(:, models{1}.vertlist+1);  % 0-start to 1-start; remove bad vertices
+            % e.g., task.diminfo{1}.models{1} ~ 'CORTEX_LEFT', count = 29696, numvert = 32492
+
+            % right cortex
+            gr = gifti(convertStringsToChars(this.ihcp_.cohort_data.surf_gii_fqfn("R")));
+            gr_pos = gr.vertices';
+            gr_pos = gr_pos(:, models{2}.vertlist+1);  % 0-start to 1-start; remove bad vertices
+            % e.g., task.diminfo{1}.models{2} ~ 'CORTEX_RIGHT', count = 29716, numvert = 32492
+
+            % subcortical
+            outinfo = cifti_diminfo_dense_get_volume_all_info(task.diminfo{1});
+            ic = mlfourd.ImagingContext2(this.ihcp_.wmparc_fqfn);
+            sc_pos = this.voxel_indices_to_position(outinfo.voxlist1, ic);
+
+            % qc:
+            % extracted = zeros(outinfo.voldims, 'single');
+            % extracted(cifti_vox2ind(outinfo.voldims, outinfo.voxlist1)) = sc.cdata(outinfo.ciftilist, 1);
+            % ifc = ic.imagingFormat; ifc.img = extracted;
+            % ifc.view
+
+            pos = [gl_pos, gr_pos, sc_pos];  % [3x29696, 3x29716, 3x31870] ~ [3x91282]
+        end
+
+        function propagated_signal = propagate_physio(this, bold_signal, physio_signal, opts)
+            %% Propagate physio_signal from physio_pos radially according to propagation velocity v.
+            %  Prior to arrival of physio signal, the array for propagated_signal will be Inf or one
+            %  according to unvisited_is_inf.
+            %  bold_signal is only needed for array sizes.
+
+            arguments
+                this mlraut.Twistors
+                bold_signal double  % N_t x N_x
+                physio_signal double  % N_t x 1
+                opts.physio_pos double = this.center_of_mass_position()  % 3 x 1
+                opts.v double {mustBeScalarOrEmpty} = this.ihcp_.v_physio  % m/s
+                opts.unvisited_is_inf logical = false
+            end
+            % physio_signal = ascol(physio_signal);
+            v = 1e3*opts.v;  % mm/s
+            
+            if v*this.ihcp_.tr > 5e4  % 50 m/s is lower limit of myelinated conduction velocity
+                propagated_signal = ones(size(bold_signal)).*physio_signal;
+                return
+            end
+
+            if opts.unvisited_is_inf
+                big = 1e3*max(physio_signal, [], "all");
+                propagated_signal = big*ones(size(bold_signal));
+            else
+                propagated_signal = physio_signal(1)*ones(size(bold_signal));
+            end
+            bold_pos = this.grayordinate_positions();  % 3 x N_x
+            x = abs(vecnorm(bold_pos - opts.physio_pos));  % Nx x 1, in mm
+
+            Nt = size(propagated_signal, 1);
+            tr = this.ihcp_.tr;
+            times = ascol(0:tr:(Nt - 1)*tr);
+            times = times + tr/2;  % mid-frame times
+            for tidx_phys = 1:Nt
+                t = (tidx_phys - 0.5)*tr;  % mid-frame time
+                tplus = t + tr/2;
+                tminus = t - tr/2;
+                selection = tminus*v <= x & x <= tplus*v;  % frame boundaries overlap
+                propagated_signal(:, selection) = ...
+                    this.forward(physio_signal, times, dt=x(selection)/v, unvisited_is_inf=opts.unvisited_is_inf);
+            end
+        end
+
+        function this = Twistors(ihcp)
+            arguments
+                ihcp mlraut.HCP
+            end
+
+            this.ihcp_ = ihcp;
+        end
+    end
+
+    methods (Static)
+        function pos = voxel_indices_to_position(v, ic)
+            %% v ~ 3 x N voxel indices (starting at 1), N >= 1
+            %  ic ~ mlfourd.ImagingContext2
+            %  pos ~ 3 x N positions in scanner coords, centered at anterior commissure
+
+            arguments
+                v {mustBeNumeric}
+                ic mlfourd.ImagingContext2
+            end
+
+            sz = size(v);
+            if sz(1) == 3
+                v1 = [v - 1; ones(1, sz(2))];
+            end
+            assert(size(v1, 1) == 4)
+
+            roi = ic.imagingFormat;
+            T = [roi.hdr.hist.srow_x; roi.hdr.hist.srow_y; roi.hdr.hist.srow_z; 0 0 0 1];
+            pos = T*v1;
+            pos = pos(1:3, :);
+        end
+
+        function signal1 = forward(signal, times, opts)
+            arguments
+                signal double  % Nt x 1
+                times double  % Nt x 1
+                opts.dt double  % 1 x Nx
+                opts.unvisited_is_inf logical = false
+            end
+            Nt = length(times);
+            Nx = length(opts.dt);
+            signal1 = ones(Nt, Nx);
+            if opts.unvisited_is_inf 
+                boundary = 1e3*max(signal, [], "all");
+            else
+                boundary = signal(1);
+            end
+            for idx = 1:Nx
+                signal1(:,idx) = interp1(times, signal, times-opts.dt(idx), "linear", boundary);
+            end
+        end
+    end
+
+    %% PRIVATE
+
+    properties (Access=private)
+        ihcp_  % mlraut.HCP
+    end
+    
+    %  Created with mlsystem.Newcl, inspired by Frank Gonzalez-Morphy's newfcn.
+end
